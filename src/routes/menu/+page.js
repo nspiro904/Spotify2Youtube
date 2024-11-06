fc54c3efdcebf23856db99fa9a3ee698ebf7e200
nspiro904 <nspiro904@gmail.com>
2024-11-06 15:32:49 -0500

    cant remember tbh

--- src/routes/menu/+page.js
+++ src/routes/menu/+page.js
@@ -1,4 +1,6 @@
-export async function load({ url }) {
+
+
+export const load = async ({ url, fetch }) => {
   const code = url.searchParams.get('code');
 
   const cid = 'f9a04e3cfd75409c9706f8067e8f1521';
@@ -8,7 +10,7 @@ export async function load({ url }) {
 
   const response = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
-    body: new URLSearchParams({ grant_type: "authorization_code", code, redirect_uri: "http://localhost:5173/menu/" }),
+    body: (new URLSearchParams({ grant_type: "authorization_code", code, redirect_uri: "http://localhost:5173/menu/" })).toString(),
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
       "Authorization": `Basic ${encodedStuff}`
@@ -19,12 +21,34 @@ export async function load({ url }) {
   const data = await response.json();
     
 
-  const fetchTracksResponse = await fetch('https://api.spotify.com/v1/me/tracks', {
+  const limit = 50;
+  
+
+  let fetchTracksResponse = await fetch(`https://api.spotify.com/v1/me/tracks?limit=${limit}`, {
     headers: {
       "Authorization": `Bearer ${data.access_token}`
     }
   })
-  const songs = await fetchTracksResponse.json();
+
+  let trackResponse = await fetchTracksResponse.json();
+
+  let songs = trackResponse.items;
     
+  let pageCounter = 1;
+
+  while(songs.length % 50 == 0) {
+
+    fetchTracksResponse = await fetch(`https://api.spotify.com/v1/me/tracks?limit=${limit}&offset=${limit * pageCounter}`, {
+      headers: {
+        "Authorization": `Bearer ${data.access_token}`
+      }
+    })
+  
+    trackResponse = await fetchTracksResponse.json();
+
+    if(trackResponse.items) songs = songs.concat(trackResponse.items);
+    ++pageCounter;
+  }
+
 return {songs};
 }
--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,8 +1,11 @@
 <script>
    export let data;
 
-   
-
-   
+   const {songs} = data;
+      
 </script>
-<h1>Migrate Songs</h1>
+<h1>Migrate {songs.length} Songs</h1>
+
+{#each songs as song }
+   <p>{song.track.name} - {song.track.artists}</p>
+{/each}
